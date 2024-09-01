c2c66ea66807ba0b764c2f2453ed32b4163c6cd6
Arnav Angarkar <arnav.angarkar20@gmail.com>
2024-09-01 08:25:42 +0530

    done

--- Problem1.c
+++ Problem1.c
@@ -3,16 +3,12 @@ int main ( void)
 {
     int i;
     int t = 0;
-    for ( i = 0 ; i < 10 ; i++)
+    for ( i = 0 ; i < 1000 ; i++)
     if ( i % 3 == 0 || i % 5 == 0)
     {
-        printf("%d " , i);
-        t=0;
-
-        t + i = t
-        printf ("%d" ,t);
-        
+        t = t+ i ;    
     }
 
-    
+    printf ("%d \n" ,t);
+ 
 }
