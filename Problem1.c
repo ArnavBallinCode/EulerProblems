ceb640aae0dac14230e39f5f5752440655199c20
Arnav Angarkar <arnav.angarkar20@gmail.com>
2024-09-01 08:20:14 +0530

    almost done

--- Problem1.c
+++ Problem1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+int main ( void)
+{
+    int i;
+    int t = 0;
+    for ( i = 0 ; i < 10 ; i++)
+    if ( i % 3 == 0 || i % 5 == 0)
+    {
+        printf("%d " , i);
+        t=0;
 
+        t + i = t
+        printf ("%d" ,t);
+        
+    }
+
+    
+}
